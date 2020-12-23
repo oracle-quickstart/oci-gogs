3cdaf4108b543da52cab100de626a60d7819dbcd
Jason Rothstein <jason.rothstein@oracle.com>
2020-12-23 18:15:16 -0500

    terraform fmt run

--- data.cloudinit_config.gogs.tf
+++ data.cloudinit_config.gogs.tf
@@ -6,11 +6,11 @@ data cloudinit_config gogs {
     content = templatefile(
       "./metadata.gogs.cloud-config",
       {
-        GOGS_DB_NAME         = var.gogs_db_name,
-        GOGS_DB_PASSWD       = var.gogs_db_passwd,
-        GOGS_DB_ROOT_PASSWD  = var.gogs_db_root_passwd,
-        GOGS_DB_USER         = var.gogs_db_user,
-        GOGS_SSH_PORT        = var.gogs_ssh_port,
+        GOGS_DB_NAME        = var.gogs_db_name,
+        GOGS_DB_PASSWD      = var.gogs_db_passwd,
+        GOGS_DB_ROOT_PASSWD = var.gogs_db_root_passwd,
+        GOGS_DB_USER        = var.gogs_db_user,
+        GOGS_SSH_PORT       = var.gogs_ssh_port,
       }
     )
   }
